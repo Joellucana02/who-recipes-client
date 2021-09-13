0a950362fdb0b3be7a3d19a214df1f57fed571c9
joel <joellucana02@gmail.com>
2021-09-13 13:43:51 -0300

    me page

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import DisplayArr from "../components/home/DisplayArr";
 const Me = () => {
-  return <div>Me</div>;
+  return (
+    <div>
+      <h2>Me</h2>
+      <div className="user-page__me">
+        <div className="me-info">
+          <img src="#" alt="null" />
+          <div>
+            <h3>Username</h3>
+            <h3>Followers</h3>
+            <h3>Following</h3>
+            <h3>Posts</h3>
+          </div>
+        </div>
+      </div>
+      <div className="me-posts">
+        <DisplayArr />
+      </div>
+    </div>
+  );
 };
 
 export default Me;
