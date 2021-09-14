b1deedb82324a4a9732618b71e0062e371198abc
joel <joellucana02@gmail.com>
2021-09-13 21:49:57 -0300

    adding styles to me page

--- src/components/home/DisplayItem.js
+++ src/components/home/DisplayItem.js
@@ -2,38 +2,35 @@ import React from "react";
 
 const DisplayItem = () => {
   return (
-    <div>
-      <h3>item</h3>
-      <div className="card">
-        <div className="card-top__info">
-          <div>
-            <img
-              src="https://randomuser.me/api/portraits/women/33.jpg"
-              alt="null"
-            />
-            <h3>Username</h3>
-          </div>
-          <div>
-            <button>FOLLOW</button>
-            <button>MORE</button>
-          </div>
+    <div className="card">
+      <div className="card-top__info">
+        <div>
+          <img
+            src="https://kctherapy.com/wp-content/uploads/2019/09/default-user-avatar-e1569863570634.png"
+            alt="null"
+          />
+          <button>USERNAME</button>
         </div>
-        <div className="card-main__content">
-          <p>
-            By default, we use the combination of a div element and a background
-            image to display the media. It can be problematic in some
-            situations. For instance, you might want to display a video or a
-            responsive image. Use the component property for these use cases:
-          </p>
+        <div>
+          <button>FOLLOW</button>
+          <button>MORE</button>
         </div>
-        <div className="card-bottom__btn">
-          <div className="card-bottom__btn-left">
-            <h3>VOTE</h3>
-            <h3>COMMENT</h3>
-          </div>
-          <div className="card-bottom__btn-right">
-            <h3>SHARE</h3>
-          </div>
+      </div>
+      <div className="card-main__content">
+        <p>
+          By default, we use the combination of a div element and a background
+          image to display the media. It can be problematic in some situations.
+          For instance, you might want to display a video or a responsive image.
+          Use the component property for these use cases:
+        </p>
+      </div>
+      <div className="card-bottom__btn">
+        <div className="card-bottom__btn-left">
+          <button>VOTE</button>
+          <button>COMMENT</button>
+        </div>
+        <div className="card-bottom__btn-right">
+          <button>SHARE</button>
         </div>
       </div>
     </div>
--- src/pages/me.js
+++ src/pages/me.js
@@ -2,17 +2,23 @@ import React from "react";
 import DisplayArr from "../components/home/DisplayArr";
 const Me = () => {
   return (
-    <div>
-      <h2>Me</h2>
-      <div className="user-page__me">
-        <div className="me-info">
-          <img src="#" alt="null" />
-          <div>
-            <h3>Username</h3>
-            <h3>Followers</h3>
-            <h3>Following</h3>
-            <h3>Posts</h3>
-          </div>
+    <div className="user-page__me">
+      <div className="me-info">
+        <div className="img-container">
+          <img
+            src="https://kctherapy.com/wp-content/uploads/2019/09/default-user-avatar-e1569863570634.png"
+            alt="null"
+          />
+        </div>
+        <h3>Username</h3>
+        <div className="me-info__stats">
+          <button>Follow</button>
+          <button>Report</button>
+        </div>
+        <div className="me-info__stats">
+          <button>Followers</button>
+          <button>Following</button>
+          <button>Posts</button>
         </div>
       </div>
       <div className="me-posts">
