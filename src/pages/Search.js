78ad9014d5368ff911fbb7a4b547b3492e683752
joel <joellucana02@gmail.com>
2021-09-23 21:58:06 -0300

    search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,65 @@
 import React from "react";
-
+import Navbar from "../components/home/Navbar";
+import { useState, useEffect } from "react";
+import axios from "axios";
 const Search = () => {
-  return <div>search</div>;
+  const [inputSearch, setInputSearch] = useState("");
+  const [resultsC, setResultsC] = useState({});
+  useEffect(() => {
+    console.log(inputSearch);
+    const getUsers = async () => {
+      const res = await axios.get(`http://localhost:3010/api/v1/users`);
+      const data = await res.data;
+      console.log(data.data);
+      setResultsC(data.data);
+    };
+    getUsers();
+  }, []);
+  function Item(props) {
+    const { data } = props;
+    return (
+      <div className="following-item">
+        <img
+          src="https://kctherapy.com/wp-content/uploads/2019/09/default-user-avatar-e1569863570634.png"
+          alt={data.username}
+        />
+        <button>{data.username}</button>
+      </div>
+    );
+  }
+  function FollowingC() {
+    return (
+      <div>
+        <h3>Results: </h3>
+
+        {resultsC.users.length > 0 ? (
+          <div>
+            <>
+              {resultsC.users.map((el) => (
+                <Item key={el._id} data={el} />
+              ))}
+            </>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Navbar />
+      <h3>Search</h3>
+      <input
+        type="text"
+        id="mySearch"
+        name="mySearch"
+        placeholder="What are you looking for?"
+        onChange={(e) => setInputSearch(e.target.value)}
+      />
+      <FollowingC />
+    </div>
+  );
 };
 
 export default Search;
